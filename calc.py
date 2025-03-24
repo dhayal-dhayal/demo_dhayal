5aae185548761d7cdec9ab59505a70612ed336a1
Dhayal-cloud <dheenadhayal33@gmail.com>
2025-03-24 21:56:20 +0530

    added calc.py

---
+++ calc.py
@@ -0,0 +1,33 @@
+import employee
+from employee import Employee
+
+#calc_employee= employee.Employee ('abc',105,106)
+#print(type(calc_employee))
+#print (isinstance(calc_employee,Employee))
+#print(calc_employee.display_employee_details())
+
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def division (x , y):
+    return round(x/y)
+print("Select calc operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Division")
+choice =  (input("Enter choice (1 or 2 or 3): "))
+
+if choice in ['1', '2','3']:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    if choice == '1':
+       # value = add(num1,num2)
+        print (add(num1,num2))
+    elif choice == '2':
+        print(subtract(num1,num2))
+    elif choice == '3':
+        print (division(num1, num2))
+
+else:
+    print("Invalid input!")
